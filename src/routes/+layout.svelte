b9f8de6cff9470d6ed8f86306b78b43bd02b2b11
Marta Rossi <marta3.rossi@mail.polimi.it>
2026-03-25 17:12:22 +0100

    add style local and global changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
@@ -9,3 +9,23 @@
 </svelte:head>
 
 {@render children()}
+
+<footer>
+	<p>©️ polimi 2026</p>
+</footer>
+
+<!-- nel file page abbiamo detto che tutti i paragrafi devono essere deep pink
+ ma svelte prende il nostro css e lo incapsula solo nell'altro file 	
+ p {
+	color: deeppink;
+	}
+-->
+
+<style>
+	:global(p) {
+		color: deeppink;
+	}
+</style>
+
+<!-- usando global tutti i paragrafi (p) sono colorati in deeppink
+-->
