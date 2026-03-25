25ddce8d5075940ad3895ba89d31aa13d744a1cc
Marta Rossi <marta3.rossi@mail.polimi.it>
2026-03-25 17:44:20 +0100

    ClicksCounter

---
+++ src/lib/components/ClicksCounter.svelte
@@ -0,0 +1,15 @@
+<script>
+    //qui posso scrivere lo javascript, quindi hava, html, css
+    // ora entriamo nel vivo dello stato, svelte tiene conto se si aggiornerà lo 0
+    //è una variabile
+    let count = $state(0)
+    let doubled = $derived(count * 2)
+</script>
+
+<h3>Clicks Counter</h3>
+
+<button onclick={() => count++}>
+clicks: {count}
+</button>
+
+<span> doubled is {doubled}</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
     //qui posso scrivere lo javascript, quindi hava, html, css
-    console.log ("hello world");
+    // ora entriamo nel vivo dello stato, svelte tiene conto se si aggiornerà lo 0
+    //è una variabile
+    import ClicksCounter from "$lib/components/ClicksCounter.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+
+<ClicksCounter />
 
 <style>
     h1 {
